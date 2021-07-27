c83acd2628891f52d560ab85fe3473e2444bcb6a
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-07-27 13:26:08 +0600

    27.07.2021

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+		<article class="content px-3 py-5 p-md-5">
+            <div class='container'>
+                  <?php 
+                        if ( have_posts() ){
+                              while ( have_posts() ){
+                                    the_post();
+                                    the_title();
+                                    the_content();
+                              }
+                        }
+                  ?>
+            </div>
+	    </article>
+<?php get_footer(); ?>
+	    
+
